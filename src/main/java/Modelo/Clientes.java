aff8bb1b04f7209cbf4c597297984ac0de100d24
jose <jmvm_@10.130.93.58>
2023-04-17 14:39:27 +0200

    creando diferentes archivos para hacer la vista clientes

--- src/main/java/Modelo/Clientes.java
+++ src/main/java/Modelo/Clientes.java
@@ -10,5 +10,17 @@ package Modelo;
  * @author jmvm_
  */
 public class Clientes {
-    
+    /*
+    //Datos de las columnas de las tablas
+      `IdCliente` int NOT NULL AUTO_INCREMENT,
+  `CIF` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `NombreCliente` varchar(300) NOT NULL,
+  `Pais` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'ESPAÑA',
+  `Mail` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Teléfono` varchar(300) DEFAULT NULL,
+  `FechaAlta` datetime DEFAULT CURRENT_TIMESTAMP,
+  `Activo` tinyint(1) DEFAULT '1',
+  `User` varchar(50) NOT NULL,
+  `Clave` varchar(20) NOT NULL,
+    */
 }
